a021422a9fc56ca16ec3af95484868d01b92abf3
liagic <leon_max@i.ua>
2026-02-13 00:24:08 +0200

    task1 and task 2 done

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,46 @@
-console.log('form');
+function getLocalStorageData(key) {
+  const data = localStorage.getItem(key);
+  if (data) {
+    try {
+      return JSON.parse(data);
+    } catch (error) {
+      return null;
+    }
+  }
+  return null;
+}
+function setDataToField() {
+  form.email.value = formData.email;
+  form.message.value = formData.message;
+}
+let formData = {
+  email: '',
+  message: '',
+};
+const form = document.querySelector('.feedback-form');
+const localStorageKey = 'feedback-form-state';
+
+form.addEventListener('input', event => {
+  if (Object.keys(formData).includes(event.target.name)) {
+    formData[event.target.name] = event.target.value;
+    localStorage.setItem(localStorageKey, JSON.stringify(formData));
+  }
+});
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  if (formData.email.trim() === '' || formData.message.trim() === '') {
+    alert('Fill please all fields');
+    return;
+  }
+  console.log(formData);
+  localStorage.removeItem(localStorageKey);
+  form.reset();
+});
+
+let dataFromLocalStorage = getLocalStorageData(localStorageKey);
+if (dataFromLocalStorage) {
+  console.log(dataFromLocalStorage);
+  formData = { ...formData, ...dataFromLocalStorage };
+  setDataToField();
+}
